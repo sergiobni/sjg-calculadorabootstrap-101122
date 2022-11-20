2e0386a9dc7bf21393d068e75f5a3045b56408ec
=Sergio <=bni.sergio@gmail.com>
2022-11-20 13:31:54 +0100

    version definitiva

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-let memoria = document.getElementById('memoria');
-let pantalla = document.getElementById('pantalla');
-let tag = document.getElementById('tag');
-let selector = document.getElementById('selector');
-let value = document.getElementById('valor');
+const memoria = document.getElementById('memoria');
+const pantalla = document.getElementById('pantalla');
+const tag = document.getElementById('tag');
+const selector = document.getElementById('selector');
+const valor = document.getElementById('valor');
 
 function element(id) {
+  const idElement = document.getElementById(id);
   if (id === 'btn=') {
-    let resultado = eval(document.getElementById('pantalla').value);
+    const resultado = eval(pantalla.value);
     memoria.textContent += `${resultado} | `;
     pantalla.value = '';
   } else if (id === 'btnc') {
     pantalla.value = '';
     memoria.textContent = '';
   } else {
-    let element = document.getElementById(id).innerHTML;
-    pantalla.value += element.trim();
+    const elementInner = idElement.innerHTML;
+    pantalla.value += elementInner.trim();
   }
-  selector.innerHTML = document.getElementById(id).type;
+  selector.innerHTML = idElement.type;
   tag.innerHTML = id;
-  value.innerHTML = document.getElementById(id).innerHTML;
+  valor.innerHTML = idElement.innerHTML;
 }
